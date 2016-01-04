a3076c4e5b541d06c98a7399bcb3ebeb5b6b72dd
Kevin <wangzefeng@huawei.com>
2016-01-03 20:25:20 -0800

    reformat node_affinity_test.go

--- plugin/pkg/scheduler/algorithm/priorities/node_affinity_test.go
+++ plugin/pkg/scheduler/algorithm/priorities/node_affinity_test.go
@@ -1,17 +1,17 @@
 /*
- Copyright 2015 The Kubernetes Authors All rights reserved.
+Copyright 2015 The Kubernetes Authors All rights reserved.
 
- Licensed under the Apache License, Version 2.0 (the "License");
- you may not use this file except in compliance with the License.
- You may obtain a copy of the License at
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
 
-     http://www.apache.org/licenses/LICENSE-2.0
+    http://www.apache.org/licenses/LICENSE-2.0
 
- Unless required by applicable law or agreed to in writing, software
- distributed under the License is distributed on an "AS IS" BASIS,
- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- See the License for the specific language governing permissions and
- limitations under the License.
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
 */
 
 package priorities
@@ -36,11 +36,41 @@ func TestNodeAffinityPriority(t *testing.T) {
 
 	affinity1 := api.Affinity{
 		HardNodeAffinity: nil,
-		SoftNodeAffinity: []api.SoftNodeAffinityTerm{{Weight: 2, MatchExpressions: []api.NodeSelectorRequirement{{Key: "foo", Operator: api.NodeSelectorOpIn, Values: []string{"bar", "value2"}}}}}}
+		SoftNodeAffinity: []api.SoftNodeAffinityTerm{
+			{
+				Weight: 2,
+				MatchExpressions: []api.NodeSelectorRequirement{
+					{Key: "foo", Operator: api.NodeSelectorOpIn, Values: []string{"bar", "value2"}},
+				},
+			},
+		},
+	}
 
 	affinity2 := api.Affinity{
 		HardNodeAffinity: nil,
-		SoftNodeAffinity: []api.SoftNodeAffinityTerm{{Weight: 2, MatchExpressions: []api.NodeSelectorRequirement{{Key: "foo", Operator: api.NodeSelectorOpIn, Values: []string{"bar"}}}}, {Weight: 4, MatchExpressions: []api.NodeSelectorRequirement{{Key: "key", Operator: api.NodeSelectorOpIn, Values: []string{"value"}}}}, {Weight: 5, MatchExpressions: []api.NodeSelectorRequirement{{Key: "key", Operator: api.NodeSelectorOpIn, Values: []string{"value"}}, {Key: "foo", Operator: api.NodeSelectorOpIn, Values: []string{"bar"}}, {Key: "az", Operator: api.NodeSelectorOpIn, Values: []string{"az1"}}}}}}
+		SoftNodeAffinity: []api.SoftNodeAffinityTerm{
+			{
+				Weight: 2,
+				MatchExpressions: []api.NodeSelectorRequirement{
+					{Key: "foo", Operator: api.NodeSelectorOpIn, Values: []string{"bar"}},
+				},
+			},
+			{
+				Weight: 4,
+				MatchExpressions: []api.NodeSelectorRequirement{
+					{Key: "key", Operator: api.NodeSelectorOpIn, Values: []string{"value"}},
+				},
+			},
+			{
+				Weight: 5,
+				MatchExpressions: []api.NodeSelectorRequirement{
+					{Key: "key", Operator: api.NodeSelectorOpIn, Values: []string{"value"}},
+					{Key: "foo", Operator: api.NodeSelectorOpIn, Values: []string{"bar"}},
+					{Key: "az", Operator: api.NodeSelectorOpIn, Values: []string{"az1"}},
+				},
+			},
+		},
+	}
 
 	tests := []struct {
 		pod          *api.Pod
